1c3e3ce0cf67736bd1d02dc1da28097df9d1f033
DarkBlub-io <risrofkingdoms@gmail.com>
2026-03-26 17:37:41 +0700

    Create data.py
    .

---
+++ data.py
@@ -0,0 +1,25 @@
+GAME = {
+    "name": "Empire Strategy",
+    "category": "Chiến lược",
+    "developer": "Victory Studio",
+    "version": "1.0.0",
+    "update_date": "23 Mar 2026",
+    "android": "Android 7.0+",
+    "description": "Empire Strategy là game chiến lược nơi bạn xây dựng đế chế và điều binh chiến đấu.",
+    "download_link": "#",
+    "play_store_link": "#",
+    "icon": "https://picsum.photos/200"
+}
+
+SCREENSHOTS = [
+    "https://picsum.photos/800/450?1",
+    "https://picsum.photos/800/450?2",
+    "https://picsum.photos/800/450?3"
+]
+
+FEATURES = [
+    "Xây dựng căn cứ và mở rộng lãnh thổ",
+    "Điều binh chiến thuật theo nhiều đội hình",
+    "Đồ họa đẹp và giao diện hiện đại",
+    "Tối ưu cho nhiều thiết bị Android"
+]
