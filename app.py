e3f4f95b84e9ee57d2905364c07890d0692b5230
DarkBlub-io <risrofkingdoms@gmail.com>
2026-03-26 17:44:46 +0700

    Update app.py
    .

--- app.py
+++ app.py
@@ -1,37 +1,15 @@
 from flask import Flask, render_template
+from data import GAME, SCREENSHOTS, FEATURES
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    game = {
-        "name": "Dark Blub",
-        "category": "Chiến Lược",
-        "developer": "AVA",
-        "version": "2.0.1",
-        "update_date": "23 Mar 2026",
-        "android": "Android 7.0+",
-        "description": "Dark Blub là tựa game Chiến Lược mang lại nhiều điều hấp dẫn thú vị.",
-        "download_link": "#",
-        "play_store_link": "#"
-    }
-
-    screenshots = [
-    "file_000000004a107209aac97fdf655a9c05.png"
-]
-
-    features = [
-        "Đồ họa đẹp và mượt",
-        "Chiến đấu chiến lược hấp dẫn",
-        "Tùy chỉnh nhân vật",
-        "Tối ưu cho nhiều máy Android"
-    ]
-
     return render_template(
         "index.html",
-        game=game,
-        screenshots=screenshots,
-        features=features
+        game=GAME,
+        screenshots=SCREENSHOTS,
+        features=FEATURES
     )
 
 if __name__ == "__main__":
